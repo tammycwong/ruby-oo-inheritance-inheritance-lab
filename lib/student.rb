cbe2b2365cfddf11459a64c69fed0f1b1f359a53
Tammy <tammycwong@gmail.com>
2021-05-02 12:11:15 -0400

    finished

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,21 @@
-class Student 
+class Student < User    
+
+    attr_accessor :knowledge
+
+    def initialize
+        @knowledge = []
+    end
+
+    #takes in a argument string of knowledge and adds it to array
+    def learn(knowledge)
+        @knowledge << knowledge
+    end
+
+    #returns knowledge array
+    def knowledge
+        @knowledge
+    end
+
+
 
 end
